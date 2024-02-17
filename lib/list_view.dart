3c3aa5bb7e1f744db864b77ac2fa661a42b55542
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-02-17 23:38:20 +0600

    Initial commit
    env setup for view things.

---
+++ lib/list_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+
+class ListviewExample extends StatelessWidget {
+  const ListviewExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
