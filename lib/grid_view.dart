8cad791a35a654dd85a7172d52667e12dd6d8ee0
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-02-18 23:10:38 +0600

    gridView

---
+++ lib/grid_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class gridViewExample extends StatelessWidget {
+  const gridViewExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('GridView'),),
+
+    );
+  }
+}
